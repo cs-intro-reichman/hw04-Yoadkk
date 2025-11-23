d5411a3e1b66a224c6ae06bc18271cce2507432a
YoadKlein <yoadklein@icloud.com>
2025-11-23 18:34:02 +0200

    This is my string baby

--- MyString.java
+++ MyString.java
@@ -18,13 +18,77 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        char[] capLet = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+        char[] lowcLet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+        
+        
+
+        String lowcString = "";
+
+        for (int i = 0 ; i < str.length() ; i++ ){
+            for (int j=0 ; j < capLet.length ; j++){
+
+                if (lowcLet[j] == str.charAt(i)){
+                    lowcString += str.charAt(i);
+
+                }
+                
+                else if (capLet[j] == str.charAt(i)){
+                    lowcString += lowcLet[j];
+                    
+                }
+
+                else if (str.charAt(i)>31 && str.charAt(i)<65 ){
+                    lowcString += str.charAt(i);
+                    break;
+
+                }
+
+                
+                    
+                
+
+
+            }
+
+        }
+        return lowcString;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
-        return false;
+        boolean doesContain = false;
+        String myString = "";
+        //unhappy happy 
+        for (int i = 0; i < str1.length(); i++) {
+            for (int j = 0; j < str2.length(); j++) {
+                
+                
+                if (str1.charAt(i) == str2.charAt(j)){
+                    myString += str2.charAt(j);
+
+
+                }
+                for (int k = 0 ; k < myString.length() ; k++){
+
+                    if (myString.length() != str2.length()){
+                        break;
+                    }
+
+                    else if (myString.charAt(k) != str2.charAt(k)){
+                        doesContain = false;
+                        break;
+                    } 
+                    else if (myString.charAt(k) == str2.charAt(k)){
+                        doesContain = true;
+                        break;
+                    }
+
+                }
+
+
+            }
+        }
+        return doesContain;
     }
 }
